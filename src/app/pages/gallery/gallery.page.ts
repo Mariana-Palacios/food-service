a88d9a4f62b870cfee252f6d70e46cd990985714
Mariana-Palacios <marianitaaax29@gmail.com>
2024-03-12 17:58:12 -0500

    feat: [V1] ✨ loading photos

--- src/app/pages/gallery/gallery.page.ts
+++ src/app/pages/gallery/gallery.page.ts
@@ -1,4 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { UserPhoto } from '@interfaces/photos'; 
 import { PhotoService } from '@services/photo.service';
 
 @Component({
@@ -7,11 +9,34 @@ import { PhotoService } from '@services/photo.service';
   templateUrl: 'gallery.page.html',
   styleUrls: ['gallery.page.scss'],
 })
-export class GalleryPage {
+export class GalleryPage implements OnInit{
 
   public photoService = inject(PhotoService)
-  constructor() {}
-  addPhotoToGallery() {
-    this.photoService.addNewToGallery();
+  public actionSheetController = inject(ActionSheetController)
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+         }
+      }]
+    });
+    await actionSheet.present();
   }
 }
