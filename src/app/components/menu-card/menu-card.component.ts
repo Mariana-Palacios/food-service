a560a718a6c3a224319d1531a80f8137e97a525d
Mariana-Palacios <marianitaaax29@gmail.com>
2024-03-13 09:59:58 -0500

    feat: [V1] ✨ creating modal and tabs

--- src/app/components/menu-card/menu-card.component.ts
+++ src/app/components/menu-card/menu-card.component.ts
@@ -1,9 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { IonCard } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu-card',
-  //standalone: true,
   templateUrl: './menu-card.component.html',
   styleUrls: ['./menu-card.component.scss']
 })
